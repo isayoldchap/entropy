f46e16ef641995a890dbde80107d207f0a90f1d8
srichardson <steve.richardson@flextrade.com>
2016-03-13 21:24:47 -0500

    Restructure the package object for ease of sharing type information

--- src/main/scala/entropy/core/package.scala
+++ src/main/scala/entropy/core/package.scala
@@ -1,51 +1,7 @@
 package com.sjr.entropy
 
-import scala.concurrent.Future
-
 /**
  * Created by stevenrichardson on 1/18/15.
  */
-package object core {
-  val A = 1
-  val B = 2
-  val C = 3
-  val D = 4
-  val E = 5
-  val F = 6
-  val G = 7
 
-  sealed trait Role
-
-  type Op[T] = Option[T]
-
-  type Fu[T] = Future[T]
-
-  object Order extends Role
-
-  object Chaos extends Role
-
-  type Move = (Point, Point) //from, to
-
-  implicit def extendedString(s: String) = new StringExt(s)
-
-  sealed trait MoveResult
-
-  object ValidMoveResult extends MoveResult
-
-  case class IllegalMoveResult(description: String) extends MoveResult
-
-  sealed trait EntropyMove
-
-  case class OrderMove(source: Point, destination: Point) extends EntropyMove
-
-  case object PassMove extends EntropyMove
-
-  case class ChaosMove(placement: Point) extends EntropyMove
-
-
-  import com.sjr.entropy.core.GameTile._
-
-  val TilesForNormalGame: Set[GameTile] = Set(RedTile, GreenTile, OrangeTile, BlueTile, WhiteTile)
-  val TilesForLargeGame: Set[GameTile]  = TilesForNormalGame ++ Set(SilverTile, YellowTile)
-
-}
+package object core extends PackageObject
---
+++ src/main/scala/entropy/core/server/package.scala
@@ -0,0 +1,13 @@
+package com.sjr.entropy.core.server
+
+import com.sjr.entropy.core.PackageObject
+
+/**
+ * This software copyright FlexTrade, Inc.
+ *
+ * @author srichardson
+ *
+ *         Date: 3/13/16
+ */
+
+package object server extends PackageObject
